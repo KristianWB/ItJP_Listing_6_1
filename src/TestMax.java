72a6aa7864ae47782d10ea46f5b082ec054cfa6f
KristianWB <42993999+KristianWB@users.noreply.github.com>
2018-09-27 16:04:29 +0200

    Initial commit

---
+++ src/TestMax.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class TestMax {
+
+    /** Starting main method */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int i = 5;
+        int j = 2;
+        int k = max(i, j);
+        System.out.println(
+                "The maximum of " + i + " and " + j + " is " + k);
+    }
+
+    /** return the max of two numbers */
+    public static int max(int num1, int num2) {
+        int result;
+
+        if (num1 > num2)
+            result = num1;
+        else
+            result = num2;
+
+        return result;
+
+
+    }
+}
